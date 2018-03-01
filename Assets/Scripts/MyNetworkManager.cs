dbba7267e98f63a163fb303da764c0f46c225774
JasonRVCC <marcmanzx@yahoo.com>
2018-03-01 18:39:48 -0500

    Random Color for each player

---
+++ Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId){
+		GameObject playerToSpawn = (GameObject)Instantiate (playerPrefab, Vector3.zero, Quaternion.identity);
+		playerToSpawn.GetComponent<Player> ().color = new Color (Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+		NetworkServer.AddPlayerForConnection (conn, playerToSpawn, playerControllerId);
+	}
+}
