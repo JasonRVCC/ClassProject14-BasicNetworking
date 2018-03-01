15a82e645335ecca178acb71cab9ec52bae36a87
JasonRVCC <marcmanzx@yahoo.com>
2018-03-01 18:49:06 -0500

    Player movement in progress

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,23 @@ public class Player : NetworkBehaviour {
 	[SyncVar]
 	public Color color;
 
+	public float moveSpeed = 1.87f;
+
 	public override void OnStartClient(){
 		gameObject.GetComponent<Renderer> ().material.color = color;
+	}
+
+	void Update(){
+		GetInput ();
+	}
+
+	private void GetInput(){
+		float x = Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime;
+		float y = Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime;
+		MoveIt (x, y);
+	}
+
+	private void MoveIt(float x, float y){
+		transform.Translate(x,y,0);
 	}
 }
