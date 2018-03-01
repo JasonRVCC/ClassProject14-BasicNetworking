8d77ab4eedbc6afa4c3798fa6816318ddae67ffa
JasonRVCC <marcmanzx@yahoo.com>
2018-03-01 18:43:29 -0500

    Player color set on start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,4 +7,8 @@ public class Player : NetworkBehaviour {
 
 	[SyncVar]
 	public Color color;
+
+	public override void OnStartClient(){
+		gameObject.GetComponent<Renderer> ().material.color = color;
+	}
 }
