6042c7354109f830b4e06f537ff4a7924eff8b34
JasonRVCC <marcmanzx@yahoo.com>
2018-03-01 19:10:31 -0500

    Bullet spawning

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Bullet : NetworkBehaviour {
+
+	[SyncVar]
+	public Color color;
+
+	public override void OnStartClient(){
+		gameObject.GetComponent<Renderer> ().material.color = color;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : NetworkBehaviour {
 	public Color color;
 
 	public float moveSpeed = 1.87f;
+	public GameObject bulletPrefab;
 
 	public override void OnStartClient(){
 		gameObject.GetComponent<Renderer> ().material.color = color;
@@ -29,6 +30,10 @@ public class Player : NetworkBehaviour {
 		} else {
 			CmdMoveIt (x, y);
 		}
+
+		if (Input.GetButtonUp ("Fire1")) {
+			CmdDoFire ();
+		}
 	}
 
 	[Command]
@@ -39,5 +44,14 @@ public class Player : NetworkBehaviour {
 	[ClientRpc]
 	public void RpcMoveIt(float x, float y){
 		transform.Translate(x,y,0);
+	}
+
+	[Command]
+	public void CmdDoFire(){
+		GameObject bullet = (GameObject)Instantiate (bulletPrefab, this.transform.position + this.transform.right, Quaternion.identity);
+		bullet.GetComponent<Rigidbody> ().velocity = Vector3.forward * 17.5f;
+		bullet.GetComponent<Bullet> ().color = color;
+		Destroy (bullet, 0.875f);
+		NetworkServer.Spawn (bullet);
 	}
 }
