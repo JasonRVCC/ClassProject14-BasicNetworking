283ef04eafb143c67993162dae85ad87c260f649
JasonRVCC <marcmanzx@yahoo.com>
2018-03-01 19:20:15 -0500

    Game Controller cs and Enemy Spawning

---
+++ Assets/Scripts/MyGameController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyGameController : NetworkBehaviour {
+
+	public GameObject enemyPrefab;
+
+	private float spawnEnemyTime = 0;
+
+	void Update () {
+		if (isServer) {
+			if (Time.fixedTime > spawnEnemyTime) {
+				SpawnEnemy ();
+			}
+		}
+	}
+
+	[Server]
+	public void SpawnEnemy(){
+		Vector3 position = new Vector3 (Random.Range (-6.75f, 6.75f), Random.Range (1f, 8f), 4.5f);
+		GameObject enemy = (GameObject)Instantiate (enemyPrefab, position, Quaternion.identity);
+		NetworkServer.Spawn (enemy);
+		spawnEnemyTime = Time.fixedTime + Random.Range (3, 8);
+	}
+}
